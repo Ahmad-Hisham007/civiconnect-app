2f6a369e542c0aee8b3bb36734bb6a8ed9efa008
Ahmad Hisham <ahhisham413@gmail.com>
2025-11-12 03:24:45 +0600

    Multi theme and routes , pages, and project structure completed

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router';
 import HomeLayout from '../Layouts/HomeLayout/HomeLayout';
 import HomePage from '../Pages/HomePage/HomePage';
+import UpcomingEvents from '../Pages/UpcomingEvents/UpcomingEvents';
+import Register from '../Pages/Register/Register';
+import Login from '../Pages/Login/Login';
+import Auth from '../Pages/Auth/Auth';
+import CreateEvents from '../Pages/CreateEvents/CreateEvents';
+import ManageEvents from '../Pages/ManageEvents/ManageEvents';
+import JoinedEvents from '../Pages/JoinedEvents/JoinedEvents';
 
 const Routes = createBrowserRouter([
     {
@@ -14,25 +21,32 @@ const Routes = createBrowserRouter([
                 element: <HomePage></HomePage>
             },
             {
-                path: "/upcoming-events"
+                path: "/upcoming-events",
+                element: <UpcomingEvents></UpcomingEvents>
             },
             {
-                path: "/register"
+                path: "/register",
+                element: <Register></Register>
             },
             {
-                path: "/login"
+                path: "/login",
+                element: <Login></Login>
             },
             {
                 path: "/auth",
+                element: <Auth></Auth>,
                 children: [
                     {
-                        path: "/auth/create-events"
+                        path: "/auth/create-events",
+                        element: <CreateEvents></CreateEvents>
                     },
                     {
-                        path: "/auth/manage-events"
+                        path: "/auth/manage-events",
+                        element: <ManageEvents></ManageEvents>
                     },
                     {
-                        path: "/auth/joined-events"
+                        path: "/auth/joined-events",
+                        element: <JoinedEvents></JoinedEvents>
                     }
                 ]
             }
